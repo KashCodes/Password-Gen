e525e9b23cb934a4c7bba759d4aa832d6a86b5df
Kash <kashay@agyeman.me>
2020-12-24 13:04:45 -0700

    started create password function

--- script.js
+++ script.js
@@ -148,13 +148,19 @@ function passwordCriteriaPrompts (){
 
 }
 
+function createPassword () {
+  var criteria = passwordCriteriaPrompts();
+  //variable to store password as it's being created
+  var results = [];
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = createPassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
